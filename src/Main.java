b602aba5f53af58738bc370a1cce93a693362b0d
CassiaFernandaMessias <cassia.fernanda.dev@gmail.com>
2024-06-09 08:15:38 -0300

    Estrutura For

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int contagem = 1; contagem <=50; contagem++) {
+System.out.println("Contagem de 1 a 50 " + contagem);
+        }
+    }
+}
